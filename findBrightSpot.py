9ed8ba13d449d8278c155a244c40b30818c707cf
Daniel Toll <ttoda@lnu.se>
2022-05-25 13:21:11 +0200

    from image to position

--- findBrightSpot.py
+++ findBrightSpot.py
@@ -1,23 +1,28 @@
 # import the necessary packages
 import numpy as np
 import cv2
+import glob
+import re
 
-# load the image and convert it to grayscale
-image = cv2.imread("images/0_s115_e111_w81.jpg")
-orig = image.copy()
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# perform a naive attempt to find the (x, y) coordinates of
-# the area of the image with the largest intensity value
-(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-cv2.circle(image, maxLoc, 5, (255, 0, 0), 2)
-# display the results of the naive attempt
-cv2.imshow("Naive", image)
-# apply a Gaussian blur to the image then find the brightest
-# region
-gray = cv2.GaussianBlur(gray, (args["radius"], args["radius"]), 0)
-(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-image = orig.copy()
-cv2.circle(image, maxLoc, args["radius"], (255, 0, 0), 2)
-# display the results of our newly improved method
-cv2.imshow("Robust", image)
-cv2.waitKey(0)
+r = 5
+
+image_list = []
+
+for filename in glob.glob('images/*.jpg'):
+    # load the image and convert it to grayscale
+    image = cv2.imread(filename)
+    orig = image.copy()
+    red_channel = image[:,:,2]
+    gray = cv2.GaussianBlur(red_channel, (r, r), 0)
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(red_channel)
+    image = orig.copy()
+    #cv2.circle(image, maxLoc, r, (255, 0, 0), 2)
+    # display the results of our newly improved method
+    #cv2.imshow("Robust", image)
+    #cv2.waitKey(0)
+
+    m = re.search(r'\w*\\\d*_s(\d*)_e(\d*)_w(\d*).jpg', filename)
+    s= float(m.group(1))
+    e= float(m.group(2))
+    w= float(m.group(3))
+    print(s,e,w, "=>", maxLoc)
