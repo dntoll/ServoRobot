17575bfbbe841413b0c6962161c9bf9473fb7296
Daniel Toll <ttoda@lnu.se>
2023-02-23 14:44:08 +0100

    changed the servor

--- RobotServer.py
+++ RobotServer.py
@@ -50,6 +50,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 while True:
                     data = conn.recv(1024)
                     if not data:
+                        print("no data baby!");
                         break
                     x,y,w = parseInput(data)
 
