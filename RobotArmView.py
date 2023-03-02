0093b0f4418d9f5873e7aeebfbfdb0bf74c8ddc1
Daniel Toll <ttoda@lnu.se>
2023-03-02 15:23:45 +0100

    .

--- RobotArmView.py
+++ RobotArmView.py
@@ -11,12 +11,12 @@ class RobotArmView(Frame):
     def __init__(self, robotArm, root):
         super().__init__()
         self.arm = robotArm
-        self.height = 256
+        self.height = 384
         self.width = 1024
-        self.leftViewMiddlePoint = (self.width/4, 256)
+        self.leftViewMiddlePoint = (self.width/4, 2* self.height /3)
         self.rightViewMiddlePoint = (3*self.width/4, self.height / 2)
         root.geometry(str(self.width) + "x" + str(self.height))
-
+        self.debug = "hej"
 
         self.lastState = robotArm.getState()
         self.scale = -5
@@ -24,6 +24,7 @@ class RobotArmView(Frame):
         self.root = root
         self.hasNewState = False
         self.gripIsOpen = False
+        self.wristIsControlled = False
 
 
     def initUI(self):
@@ -34,9 +35,13 @@ class RobotArmView(Frame):
         self.canvas.bind('<B1-Motion>', self.mouseMoveWithButtonDown)
         self.canvas.bind('<Button-1>', self.mouseMoveWithButtonDown)
         self.canvas.bind('<MouseWheel>', self.wrist)
+        self.canvas.bind('<Button-2>', self.stopWristFromControl)
         self.canvas.bind('<Button-3>', self.grip)
         self.canvas.pack(fill=BOTH, expand=1)    
     
+    def stopWristFromControl(self, event):
+        self.wristIsControlled = False
+
     def wrist(self, event):
         
         distanceMove = 0.17
@@ -45,7 +50,7 @@ class RobotArmView(Frame):
         else:
             self.lastState.wristWorldAngleRadians = self.arm.getState().wristWorldAngleRadians -distanceMove
 
-
+        self.wristIsControlled = True
         self.hasNewState = True
 
         print("arm state", self.arm.getState().wristWorldAngleRadians, flush=True)
@@ -64,10 +69,18 @@ class RobotArmView(Frame):
         abs_coord_x = self.root.winfo_pointerx() - self.root.winfo_x() -10
         abs_coord_y = self.root.winfo_pointery() - self.root.winfo_y() - 30
 
+        if self.wristIsControlled is False:
+
+            ydiff = self.leftViewMiddlePoint[1]-150 - abs_coord_y
+            xdiff = self.leftViewMiddlePoint[0] - abs_coord_x
+            angle = math.atan2(ydiff, xdiff)
+            self.lastState.wristWorldAngleRadians = angle
 
         if abs_coord_x < self.width/2:
             robotX = (abs_coord_x - self.leftViewMiddlePoint[0])/self.scale
             robotY = (abs_coord_y - self.leftViewMiddlePoint[1])/self.scale
+
+            
 
             self.lastState.distanceFromBase = robotX
             self.lastState.heightOverBase = robotY
@@ -83,10 +96,15 @@ class RobotArmView(Frame):
             while rotationAngle < 0:
                 rotationAngle += 6.28
 
+                
+
             self.lastState.rotationRadians = rotationAngle
             self.lastState.distanceFromBase = distance
             self.hasNewState = True
+        
 
+        if self.lastState.distanceFromBase < 0:
+            self.lastState.distanceFromBase = 0
 
     def draw(self):
         lap = self.arm.lowerArmBone.getPos()
@@ -100,6 +118,9 @@ class RobotArmView(Frame):
         self.canvas.delete('all')
 
         start_point = self.leftViewMiddlePoint
+
+        #Ground
+        self.canvas.create_line(0, start_point[1]-self.scale*5, self.width/2, start_point[1] -self.scale*5)
         
         for bone in bones:
             bone = bone.mul(self.scale).add(Vector3(self.leftViewMiddlePoint[0], self.leftViewMiddlePoint[1], 0))
@@ -108,15 +129,29 @@ class RobotArmView(Frame):
             thickness = 1
             self.canvas.create_line(start_point[0], start_point[1], end_point[0], end_point[1])
             start_point = end_point
+
+            
         
-        
+        pos = str(int(wp.x*10)/10) + ", " + str(int(wp.y*10)/10)
+        self.canvas.create_text(start_point[0], start_point[1]-30, text=pos, fill="black", font=('Helvetica 9'))
         start_point = self.rightViewMiddlePoint
         
+
+        v = -60/360/(2.0*math.pi)
+        len = 30 * self.scale
+        end_point = (self.rightViewMiddlePoint[0] +  math.cos(v) * len, math.sin(v) * len + self.rightViewMiddlePoint[1])
+        self.canvas.create_line(start_point[0], start_point[1], end_point[0], end_point[1])
+
         #Draw a clock like directional 
         v = -self.arm.rotate.getAngleRadians()
         len = self.arm.getState().distanceFromBase * self.scale
         end_point = (self.rightViewMiddlePoint[0] +  math.cos(v) * len, math.sin(v) * len + self.rightViewMiddlePoint[1])
         self.canvas.create_line(start_point[0], start_point[1], end_point[0], end_point[1])
+        pos = str(self.arm.rotate.getAngleRadians())
+        self.canvas.create_text(start_point[0], start_point[1]-30, text=pos, fill="black", font=('Helvetica 9'))
+
+        self.canvas.create_text(20, 20, text=self.debug, fill="red", font=('Helvetica 9 bold'))
+             
     
         self.root.update_idletasks()
         self.root.update()
