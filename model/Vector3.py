cd39d570d570b9e273cb3e545a228b5119363a56
Daniel Toll <ttoda@lnu.se>
2023-03-13 13:31:56 +0100

    .

--- model/Vector3.py
+++ model/Vector3.py
@@ -12,7 +12,6 @@ class Vector3:
     
     def add(self, other):
         return Vector3(self.x + other.x,  self.y + other.y , self.z + other.z)
-    
 
     def __str__(self):
         return "[{:.2f}".format(self.x) + ", {:.2f}".format(self.y) + ", {:.2f}]".format(self.z)
